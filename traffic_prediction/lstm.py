e9823f59a165d4224bfae824f89ba10299c09c2c
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2022-11-20 18:30:10 +0530

    reading data

---
+++ traffic_prediction/lstm.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+from sklearn import linear_model
+from sklearn import *
+
+
+trafficData=pd.read_csv('D:\\Acedamic\\FYP\\Codes\\Datasets\\tdata.csv', usecols=[2])
