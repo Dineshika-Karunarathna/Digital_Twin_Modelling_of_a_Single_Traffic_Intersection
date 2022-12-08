16fbb6d24902cdf1b271b639bb5f2e778fd379ef
Gangitha Diwanajana <diwanjanagangitha@gmail.com>
2022-12-08 09:35:42 +0530

    Assign variables

--- Sumo.py
+++ Sumo.py
@@ -9,3 +9,12 @@ import matplotlib.animation as animation
 import random
 from geographiclib.geodesic import Geodesic
 import time
+
+geod = Geodesic.WGS84
+G = ox.graph_from_bbox(6.92216, 6.92019, 79.87877, 79.87732, network_type='drive') #, simplify=True)
+G = ox.add_edge_speeds(G)
+G = ox.add_edge_travel_times(G)
+
+projected_graph = ox.project_graph(G, to_crs="EPSG:3395")
+Gc = ox.consolidate_intersections(projected_graph, dead_ends=True)
+edges = ox.graph_to_gdfs(ox.get_undirected(Gc), nodes=False)
