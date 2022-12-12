de937bede48443013e5ef09dae79c932a0a9ca40
Gangitha Diwanajana <diwanjanagangitha@gmail.com>
2022-12-12 22:42:40 +0530

    Add for loop

--- Sumo.py
+++ Sumo.py
@@ -39,3 +39,37 @@ RIGHT_SIG = ""
 columns = ['vehID', 'subroute', 'speed', 'turn', 'angle', 'lengthOfSubroute']
 data = []
 df = pd.DataFrame()
+
+for iroute in range(vehnumber):
+    
+    try:
+        y0 = 0.0
+        x0 = 0.0
+        
+        lat = round(random.uniform(6.92019, 6.92216), 5)
+        lon = round(random.uniform(79.87877, 79.87732), 5)
+        good_orig_node = ox.get_nearest_node(G, (lat, lon), method='euclidean')
+
+        lat = round(random.uniform(6.92019, 6.92216), 5)
+        lon = round(random.uniform(79.87877, 79.87732), 5)
+        good_dest_node = ox.get_nearest_node(G, (lat, lon), method='euclidean')
+
+        routep1 = nx.shortest_path(G, good_orig_node, good_dest_node)
+        lengthOfRoute1 = nx.shortest_path_length(G, good_orig_node, good_dest_node, weight='length')
+
+        lat = round(random.uniform(6.92019, 6.92216), 5)
+        lon = round(random.uniform(79.87877, 79.87732), 5)
+        next_dest_node = ox.get_nearest_node(G, (lat, lon), method='euclidean')
+
+        routep2 = nx.shortest_path(G, good_dest_node, next_dest_node)
+        lengthOfRoute2 = nx.shortest_path_length(G, good_dest_node, next_dest_node, weight='length')
+
+        route = routep1 + routep2
+        lengthOfRoute = lengthOfRoute1 + lengthOfRoute2
+        
+        lor = round(lengthOfRoute/1000.0, 2)
+        
+        angleList = []
+        direzione = []
+        route_roadnames = []
+        route_speed = []
