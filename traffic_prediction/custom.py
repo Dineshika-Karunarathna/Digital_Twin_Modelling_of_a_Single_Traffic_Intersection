5966c03ff9976adaea9f9252834445c0f8299f65
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2022-12-30 06:01:46 +0530

    created model class

--- traffic_prediction/custom.py
+++ traffic_prediction/custom.py
@@ -24,3 +24,16 @@ training_images = read_images('data/FashionMNIST/raw/train-images-idx3-ubyte.gz'
 test_images = read_images('data/FashionMNIST/raw/t10k-images-idx3-ubyte.gz', image_size, num_test)
 training_labels = read_labels('data/FashionMNIST/raw/train-labels-idx1-ubyte.gz', num_train)
 test_labels = read_labels('data/FashionMNIST/raw/t10k-labels-idx1-ubyte.gz', num_test)
+
+class NeuralNetwork(tf.keras.Model):
+  def __init__(self):
+    super(NeuralNetwork, self).__init__()
+    self.sequence = tf.keras.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(20, activation='relu'),
+      tf.keras.layers.Dense(10)
+    ])
+
+  def call(self, x: tf.Tensor) -> tf.Tensor:
+    y_prime = self.sequence(x)
+    return y_prime
