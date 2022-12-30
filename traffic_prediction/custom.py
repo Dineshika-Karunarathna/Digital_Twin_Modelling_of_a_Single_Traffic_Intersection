a38a22f06a8d0d9b1ae1cd69227f7b038de9e55f
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2022-12-30 05:41:28 +0530

    load data

--- traffic_prediction/custom.py
+++ traffic_prediction/custom.py
@@ -15,3 +15,12 @@ def read_labels(path: str, num_items: int) -> np.ndarray:
     data = np.frombuffer(file.read(num_items + 8), np.uint8, offset=8)
     data = data.astype(np.int64)
   return data
+
+image_size = 28
+num_train = 60000
+num_test = 10000
+
+training_images = read_images('data/FashionMNIST/raw/train-images-idx3-ubyte.gz', image_size, num_train)
+test_images = read_images('data/FashionMNIST/raw/t10k-images-idx3-ubyte.gz', image_size, num_test)
+training_labels = read_labels('data/FashionMNIST/raw/train-labels-idx1-ubyte.gz', num_train)
+test_labels = read_labels('data/FashionMNIST/raw/t10k-labels-idx1-ubyte.gz', num_test)
