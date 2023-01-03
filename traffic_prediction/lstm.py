0c59f06d83f527a7d653213ab983dcb2309fee36
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-01-03 21:27:18 +0530

    split data squence

--- traffic_prediction/lstm.py
+++ traffic_prediction/lstm.py
@@ -16,3 +16,17 @@ train_size=int(len(dataset)*0.7)
 test_size=len(dataset)-train_size
 train,test=dataset[0:train_size,:],dataset[train_size:len(dataset),:] 
 print (len(train),len(test))   
+
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return np.array(X), np.array(y)
