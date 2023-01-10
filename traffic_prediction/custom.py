c359de959e4a19f134530b575204ce4fabff0737
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-01-10 21:10:20 +0530

    add prediction layers

--- traffic_prediction/custom.py
+++ traffic_prediction/custom.py
@@ -48,3 +48,16 @@ model.summary()
 
 model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
 model.fit(training_images, training_labels, epochs=5)
+
+class NeuralNetwork(tf.keras.Model):
+  def __init__(self):
+    super(NeuralNetwork, self).__init__()
+    self.sequence = tf.keras.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(20, activation='relu'),
+      tf.keras.layers.Dense(10)
+    ])
+
+  def call(self, x: tf.Tensor) -> tf.Tensor:
+    y_prime = self.sequence(x)
+    return y_prime
