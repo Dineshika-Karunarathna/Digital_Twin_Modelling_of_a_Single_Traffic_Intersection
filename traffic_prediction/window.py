8ec0daac265388dd44b135cf5cc86c1aff149b86
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-01-15 20:07:25 +0530

    constructor

---
+++ traffic_prediction/window.py
@@ -0,0 +1,8 @@
+class WindowSplit():
+  def __init__(self, input_width, label_width, shift,
+               train_df=train_df, val_df=val_df, test_df=test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
