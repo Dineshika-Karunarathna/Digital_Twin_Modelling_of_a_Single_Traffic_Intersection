c3c73d3945c54be88aa377a2f0d428a235733e75
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-01-15 20:15:42 +0530

    setting lable column indexes

--- traffic_prediction/window.py
+++ traffic_prediction/window.py
@@ -6,3 +6,10 @@ class WindowSplit():
     self.train_df = train_df
     self.val_df = val_df
     self.test_df = test_df
+
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
