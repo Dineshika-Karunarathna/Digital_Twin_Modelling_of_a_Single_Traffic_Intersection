2677fda64a7482ddeecbabd2392f6437313fe03b
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-02-25 20:41:24 +0530

    scaling data

--- traffic_prediction/Models/Bidrectional.py
+++ traffic_prediction/Models/Bidrectional.py
@@ -12,3 +12,20 @@ trafficData=pd.read_csv('/gdrive/MyDrive/ML Tutorial/tdata.csv', usecols=[2])
 dataset=trafficData.values
 dataset=dataset.astype('float32')
 from sklearn.preprocessing import MinMaxScaler
+
+
+scaler = MinMaxScaler(feature_range=(0, 1)) #Also try QuantileTransformer
+dataset = scaler.fit_transform(dataset)
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
