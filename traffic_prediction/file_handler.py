01e1dfa69ed043dd347b7bc52c2bbb8d1e9bf5ba
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-06-03 19:47:29 +0530

    solved dependancy error

--- traffic_prediction/file_handler.py
+++ traffic_prediction/file_handler.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 def get_file_path(file_name):
     """Returns the file path of the given file name."""
@@ -17,3 +18,11 @@ def write_data(file_name, data):
     """Writes the given data to the given file name."""
     with open(get_file_path(file_name), 'w') as file:
         file.write(data)
+def get_data(file_name):
+    """Returns the content of the given file name."""
+    with open(get_file_path(file_name), 'r') as file:
+        traffic_data = pd.read_csv(file)
+        dataset=traffic_data.values
+        #dataset=dataset.astype('float32')
+
+    return dataset
--- traffic_prediction/multy_lstm.py
+++ traffic_prediction/multy_lstm.py
@@ -1,8 +1,15 @@
 from numpy import array
 import numpy as np
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
 import pandas as pd
 import matplotlib.pyplot as plt
+import file_handler
 
+
+
+dataset=file_handler.get_data('cleanedkaggleTrafic.csv')
+
+print(dataset)
