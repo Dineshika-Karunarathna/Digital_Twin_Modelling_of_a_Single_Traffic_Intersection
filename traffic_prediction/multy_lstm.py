62d546b6502c758aa2dddb461eb7de30dc331d08
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-06-03 20:46:59 +0530

    splitting order changed

--- traffic_prediction/multy_lstm.py
+++ traffic_prediction/multy_lstm.py
@@ -7,6 +7,8 @@ from keras.layers import Dense
 import pandas as pd
 import matplotlib.pyplot as plt
 import file_handler
+import scaler
+import split_data
 
 
 
@@ -16,3 +18,14 @@ print(dataset)
 plt.plot(dataset)
 plt.show()
 print(len(dataset))
+
+dataset= scaler.scale_data(dataset)
+print(dataset)
+
+n_steps=24
+
+train_size = int(len(dataset) * 0.66)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+trainX, trainY = split_data.split_sequence(train, n_steps)
+testX, testY = split_data.split_sequence(test, n_steps)
--- traffic_prediction/scaler.py
+++ traffic_prediction/scaler.py
@@ -6,3 +6,7 @@ def scale_data(dataset):
     dataset = scaler.fit_transform(dataset)
     return dataset
 
+def inverse_scale_data(dataset):
+    scaler = MinMaxScaler(feature_range=(0, 1)) #Also try QuantileTransformer
+    dataset = scaler.inverse_transform(dataset)
+    return dataset
