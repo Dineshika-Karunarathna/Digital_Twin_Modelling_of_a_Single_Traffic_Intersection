6dfebcc1b94b85b86269f7c3c461e9346d8c9d05
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-06-04 21:43:59 +0530

    bug fixed

--- traffic_prediction/6_phase.py
+++ traffic_prediction/6_phase.py
@@ -14,8 +14,16 @@ import model_eval
 
 
 
-traffic_data = pd.read_csv('3_way.csv',usecols=[2])
+traffic_data = pd.read_csv('D:\\Acedamic\\FYP\\Codes\\Datasets\\3_way.csv')
 traffic_data.dropna(inplace=True)
 dataset=traffic_data.values
 
 print(dataset)
+N_S=traffic_data['N_S']
+N_W=traffic_data['N_W']
+S_N=traffic_data['S_N']
+S_W=traffic_data['S_W']
+W_N=traffic_data['W_N']
+W_S=traffic_data['W_S']
+
+print(N_W)
