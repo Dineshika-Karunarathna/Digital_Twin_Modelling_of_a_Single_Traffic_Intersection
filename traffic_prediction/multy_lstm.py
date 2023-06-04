ccca94e8a740e433c5d226b75fed5d0151f99cc5
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-06-04 09:14:33 +0530

    reshape input vector

--- traffic_prediction/multy_lstm.py
+++ traffic_prediction/multy_lstm.py
@@ -29,3 +29,24 @@ test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 trainX, trainY = split_data.split_sequence(train, n_steps)
 testX, testY = split_data.split_sequence(test, n_steps)
+
+print(trainX.shape, trainY.shape)
+print(testX.shape, testY.shape)
+
+
+# define input sequence
+#raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# choose a number of time steps
+n_steps = 24
+# split into samples
+X, y = split_data.split_sequence(dataset, n_steps)
+# reshape from [samples, timesteps] into [samples, timesteps, features]
+n_features = 1
+trainX = trainX.reshape((trainX.shape[0], trainX.shape[1], n_features))
+testX = testX.reshape((testX.shape[0], testX.shape[1], n_features))
+print(trainX.shape, trainY.shape)
+print(testX.shape, testY.shape)
+
+testY_flat=testY.reshape(-1)
+plt.plot(testY_flat)
+plt.show()
