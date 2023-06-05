2a7f6f595298b9ca6716b4571f6e6b19ae8e9adc
Janith Chandula <63604397+JanithChandula@users.noreply.github.com>
2023-06-05 14:52:06 +0530

    reordered phases

--- traffic_prediction/6_phase.py
+++ traffic_prediction/6_phase.py
@@ -28,15 +28,39 @@ scaled_traffic_data = pd.DataFrame(scaled_data, columns=traffic_data.columns)
 print(type(scaled_traffic_data))
 
 '''
-N_S=traffic_data[['N_S']].values
-N_S=traffic_data[['N_S']].values
-N_W=traffic_data[['N_W']].values
-S_N=traffic_data[['S_N']].values
-S_W=traffic_data[['S_W']].values
-W_N=traffic_data[['W_N']].values
-W_S=traffic_data[['W_S']].values
-scaler1 = MinMaxScaler(feature_range=(0, 1))
-N_S = scaler1.fit_transform(N_S)
+data=[]
+for i, phase in enumerate(['N_S', 'N_W', 'S_N', 'S_W', 'W_N', 'W_S']):
+    data.append(traffic_data[phase].values)
+
+
+# N_S=traffic_data[['N_S']].values
+#N_W=traffic_data[['N_W']].values
+# S_N=traffic_data[['S_N']].values
+# S_W=traffic_data[['S_W']].values
+# W_N=traffic_data[['W_N']].values
+# W_S=traffic_data[['W_S']].values  
+#print(N_W)
+Scalers=[]
+for i in range(6):
+    Scalers.append(MinMaxScaler(feature_range=(0, 1)))
+# scaler1 = MinMaxScaler(feature_range=(0, 1))
+# scaler2 = MinMaxScaler(feature_range=(0, 1))
+# scaler3 = MinMaxScaler(feature_range=(0, 1))
+# scaler4 = MinMaxScaler(feature_range=(0, 1))
+# scaler5 = MinMaxScaler(feature_range=(0, 1))
+# scaler6 = MinMaxScaler(feature_range=(0, 1))
+print(Scalers[0])
+
+for i in range(6):
+    data[i]=Scalers[i].fit_transform(data[i].reshape(-1,1))
+# N_S = scaler1.fit_transform(data[0].reshape(-1,1))
+# N_W = scaler2.fit_transform(data[1].reshape(-1,1))
+# S_N = scaler3.fit_transform(data[2].reshape(-1,1))
+# S_W = scaler4.fit_transform(data[3].reshape(-1,1))
+# W_N = scaler5.fit_transform(data[4].reshape(-1,1))
+# W_S = scaler6.fit_transform(data[5].reshape(-1,1))
+
+
 
 
 
@@ -46,7 +70,7 @@ N_S = scaler1.fit_transform(N_S)
 ##############################################################################################
 
 
-
+N_S=data[0]
 
 
 
@@ -105,81 +129,101 @@ trainX=[]
 trainY=[]
 testX=[]
 testY=[]
-models=[]
+models = {}
+predictions = {}
 Train_Predict=[]
 Test_Predict=[]
-
-trainx,trainy,testx,testy=split(n_steps,N_S)
-trainX.append(trainx)
-trainY.append(trainy)
-testX.append(testx)
-testY.append(testy)
-model=lstm_mod.stacked_train(learning_rate, batch_size ,epochs,n_steps,n_features,trainX[0],trainY[0])
-
-models.append(model)
-print(trainX[0].shape)
-print(model.summary())
+for i, phase in enumerate(['N_S', 'N_W', 'S_N', 'S_W', 'W_N', 'W_S']):
+    trainx,trainy,testx,testy=split(n_steps,data[i])
+    trainX.append(trainx)
+    trainY.append(trainy)
+    testX.append(testx)
+    testY.append(testy)
 
 
 
-# trainX,trainY,testX,testY=split(n_steps,N_S)
-# model=lstm_mod.stacked_train(learning_rate, batch_size ,epochs,n_steps,n_features,trainX,trainY)
-train_predict=model.predict(trainX[0])
-print("train Predict")
-print(train_predict.shape)
-print(train_predict)
-Train_Predict.append(train_predict)
-test_predict=model.predict(testX[0])
-Test_Predict.append(test_predict)
-print(test_predict)
+for i, phase in enumerate(['N_S', 'N_W', 'S_N', 'S_W', 'W_N', 'W_S']):
+    model=lstm_mod.stacked_train(learning_rate, batch_size ,epochs,n_steps,n_features,trainX[i],trainY[i])
+    models[phase] = model
+    print(trainX[i].shape)
+    print(model.summary())
+    train_predict=model.predict(trainX[i])
+    print("train Predict")
+    print(train_predict.shape)
+    print(train_predict)
+    Train_Predict.append(train_predict)
+    test_predict=model.predict(testX[i])
+    Test_Predict.append(test_predict)
+    print(test_predict)
 
-Scaled_trainPredict = scaler1.inverse_transform(Train_Predict[0])
-Scaled_trainY = scaler1.inverse_transform(trainY[0])
-Scaled_testPredict = scaler1.inverse_transform(Test_Predict[0])
-Scaled_testY = scaler1.inverse_transform(testY[0])
-
-trainY_flat=Scaled_trainY.reshape(-1)
-plt.plot(trainY_flat)
-
-trainPredict_flat=Scaled_trainPredict.reshape(-1)
-plt.plot(trainPredict_flat)
-plt.legend('Ground Turth','Prediction')
-plt.show()
+    Scaled_trainPredict = Scalers[i].inverse_transform(Train_Predict[i])
+    Scaled_trainY = Scalers[i].inverse_transform(trainY[i])
+    Scaled_testPredict = Scalers[i].inverse_transform(Test_Predict[i])
+    Scaled_testY = Scalers[i].inverse_transform(testY[i])
 
 
-plt.figure(figsize=(10, 5))
-plt.plot(Scaled_trainY[:48], label="Ground Turth")
-plt.plot(trainPredict_flat[:48],label="Prediction")
-plt.legend(loc="upper left")
+    trainY_flat=Scaled_trainY.reshape(-1)
+    plt.plot(trainY_flat)
 
-plt.show()
+    trainPredict_flat=Scaled_trainPredict.reshape(-1)
+    predictions[phase] = trainPredict_flat
+    plt.plot(trainPredict_flat)
+    plt.legend('Ground Turth','Prediction')
+    plt.show()
 
-testY_flat=Scaled_testY.reshape(-1)
-plt.plot(testY_flat)
-testPredict_flat=Scaled_testPredict.reshape(-1)
-plt.plot(testPredict_flat)
-plt.legend('Ground Turth','Prediction')
-plt.legend(loc="upper left")
 
-plt.show()
+    plt.figure(figsize=(10, 5))
+    plt.plot(Scaled_trainY[:48], label="Ground Turth")
+    plt.plot(trainPredict_flat[:48],label="Prediction")
+    plt.legend(loc="upper left")
 
-plt.figure(figsize=(10, 5))
+    plt.show()
 
-plt.plot(Scaled_testY[:48],label="Ground Turth")
-plt.plot(testPredict_flat[:48],label="Prediction")
-#plt.xlabel('Hours')
-plt.ylabel('Traffic')
-plt.legend(loc="upper left")
+    testY_flat=Scaled_testY.reshape(-1)
+    plt.plot(testY_flat)
+    testPredict_flat=Scaled_testPredict.reshape(-1)
+    plt.plot(testPredict_flat)
+    plt.legend('Ground Turth','Prediction')
+    plt.legend(loc="upper left")
 
-plt.show()
+    plt.show()
 
-print(model.summary())
+    plt.figure(figsize=(10, 5))
 
-print('Train')
-model_eval.mae(Scaled_trainY,trainPredict_flat)
-model_eval.mse(Scaled_trainY,trainPredict_flat)
-model_eval.rmse(Scaled_trainY,trainPredict_flat)
-print('Test')
-model_eval.mae(Scaled_testY,testPredict_flat)
-model_eval.mse(Scaled_testY,testPredict_flat)
-model_eval.rmse(Scaled_testY,testPredict_flat)
+    plt.plot(Scaled_testY[:48],label="Ground Turth")
+    plt.plot(testPredict_flat[:48],label="Prediction")
+    #plt.xlabel('Hours')
+    plt.ylabel('Traffic')
+    plt.legend(loc="upper left")
+
+    plt.show()
+
+    print(model.summary())
+
+    print('Train')
+    model_eval.mae(Scaled_trainY,trainPredict_flat)
+    model_eval.mse(Scaled_trainY,trainPredict_flat)
+    model_eval.rmse(Scaled_trainY,trainPredict_flat)
+    print('Test')
+    model_eval.mae(Scaled_testY,testPredict_flat)
+    model_eval.mse(Scaled_testY,testPredict_flat)
+    model_eval.rmse(Scaled_testY,testPredict_flat)
+
+
+        
+
+
+
+
+
+
+
+
+
+    # trainX,trainY,testX,testY=split(n_steps,N_S)
+    # model=lstm_mod.stacked_train(learning_rate, batch_size ,epochs,n_steps,n_features,trainX,trainY)
+
+
+
+
+
